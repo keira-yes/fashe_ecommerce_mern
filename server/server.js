e902c20c02d0ad2122432173190b7416509b84b4
Kirillova <keira.kirillova@gmail.com>
2021-03-05 18:07:03 +0200

    add api get products, product by id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const products = require('./data/products');
 
 const app = express();
 app.listen(5000, console.log('server is running on 5000 port'));
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(item => item.id === req.params.id);
+  res.json(product);
+});
